7526995dfab3b052f877f3a581ef113625c18130
Trish Ogen <patriciaogen@gmail.com>
2019-11-03 18:56:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :authors, only: [:show, :new, :create, :edit, :update]
+  resources :posts, only: [:show, :new, :create, :edit, :update]
+
 end
